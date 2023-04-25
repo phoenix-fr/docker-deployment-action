db1ec24a92b6a2a335e4af86d41aa6829107639b
rschiano-phoenix <130684839+rschiano-phoenix@users.noreply.github.com>
2023-04-25 19:28:51 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -81,7 +81,7 @@ eval $(ssh-agent)
 ssh-add "$HOME/.ssh/id_rsa"
 
 echo "Add known host..."
-ssh-keyscan -H $INPUT_REMOTE_DOCKER_HOST >> ~/.ssh/known_hosts
+ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
 
 if ! [ -z "$INPUT_DOCKER_PRUNE" ] && [ $INPUT_DOCKER_PRUNE = 'true' ] ; then
   yes | docker --log-level debug --host "ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT" system prune -a 2>&1
